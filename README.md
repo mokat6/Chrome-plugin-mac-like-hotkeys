f3cc8a277475e9b68a498d861671d96f4f1b73df
Momo <github2@5208866.xyz>
2025-02-27 22:10:27 +0200

    added most features

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # hotkeys-thumb-action
+
 Chrome plugin - hotkeys like on Mac keyboard
+Use Alt+T to open a new tab (ctrl + T still works)
+
+# Hotkey List
+
+- Close tab
+- New tab
+- Open new window (alt + N)
+- Switch to next window (alt + ,)
+- Switch to prev window (alt + .)
+- Reload tab
+- Switch to tab 1 (alt + 1)
+- Switch to tab 2 (alt + 2)
+- Switch to tab 3
+- Switch to tab 4
+- Switch to tab 5
+- Switch to tab 6
+- Switch to tab 7
+- Switch to tab 8
+- Switch to the last tab (alt + 9)
+
+- Find on page - doesn't work (can't be done)
+- Focus on URL - doesn't work (can't be done)
+
+# Usage
+
+1. Install the extension.
+2. Open edge://extensions/shortcuts
+3. Set your hotkeys
+
+# Why is it better than other extensions?
+
+works on settings tabs, new tab, etc
+
+Limitation: doesn't work on popped out admin tools window.
+    
+# Default keys
+
+Max number of default key suggestions is 4
+For the remaining hotkeys set it yourself
---
+++ app/service_worker.js
@@ -0,0 +1,107 @@
+"use strict";
+
+chrome.commands.onCommand.addListener(function (command) {
+  switch (command) {
+    case "new_tab":
+      chrome.tabs.create({ url: "chrome://newtab" });
+      break;
+
+    case "reload_page":
+      chrome.tabs.reload();
+      break;
+
+    case "close_tab":
+      console.log("closing the tab lol")
+      chrome.tabs
+        .query({ currentWindow: true, active: true })
+        .then(function (tab) {
+          console.log("the tab is  > ", tab)
+
+          if (tab.length === 0) {
+            // this is the case when the user tries to close a popped out separate window of DevTools
+            // doesn't work, no way to get that window. Doesn't show up in chrome.windows.getAll()
+          }
+
+          // can't close "new tab", hotfix: change url, then close
+          if (!tab.url || tab.url.includes("://newtab")) {
+            chrome.tabs.update(tab[0].id, { url: "about:blank" }, function () {
+              chrome.tabs.remove(tab[0].id);
+            });
+          } else {
+            chrome.tabs.remove(tab[0].id);
+          }
+        });
+      break;
+
+    case "find_on_page":
+      break;
+
+    case "focus_on_url":
+      break;
+
+    case "new_window":
+      chrome.windows.create();
+
+      break;
+    case "next_window":
+      switchWindow(1);
+
+      break;
+    case "prev_window":
+      switchWindow(-1);
+
+      break;
+    case "tab1":
+      switchToTab(0);
+      break;
+    case "tab2":
+      switchToTab(1);
+      break;
+    case "tab3":
+      switchToTab(2);
+      break;
+    case "tab4":
+      switchToTab(3);
+      break;
+    case "tab5":
+      switchToTab(4);
+      break;
+    case "tab6":
+      switchToTab(5);
+      break;
+    case "tab7":
+      switchToTab(6);
+      break;
+    case "tab8":
+      switchToTab(7);
+      break;
+    case "tab_last":
+      switchToTab();
+      break;
+  }
+});
+
+function switchToTab(n) {
+  chrome.tabs.query({ currentWindow: true }).then((tabs) => {
+    const index = n ?? tabs.length - 1;
+    if (tabs.length === 0 || index >= tabs.length) return;
+
+    const tabId = tabs.find((tab) => tab.index == index)?.id;
+    chrome.tabs.update(tabId, { active: true });
+  });
+}
+
+function switchWindow(direction) {
+  chrome.windows.getAll({ populate: false }).then((windows) => {
+    if (windows.length < 2) return;
+
+    chrome.windows.getCurrent().then((current) => {
+      windows.sort((a, b) => a.id - b.id);
+      let index = windows.findIndex((win) => win.id === current.id);
+      if (index === -1) return;
+
+      index = (index + direction + windows.length) % windows.length;
+      chrome.windows.update(windows[index].id, { focused: true });
+    });
+  });
+}
